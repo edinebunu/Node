e6db9083ecacc8bf666233c71322f4e002de905d
Biro Edmond <edmonebiro502@yahoo.com>
2020-07-01 14:01:34 +0300

    v0

---
+++ app/src/main/java/com/example/nodedpit/WelcomePage.java
@@ -0,0 +1,42 @@
+package com.example.nodedpit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomePage extends AppCompatActivity {
+
+
+    private static final String TAG = "WelcomePage";
+    Button SignUp;
+    Button LogIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+
+        SignUp = (Button) findViewById(R.id.signup);
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomePage.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        LogIn = (Button) findViewById(R.id.login);
+
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomePage.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
