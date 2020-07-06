f244b2d85b10fea01eaec6d9ee404a6c79419bdf
Rares03 <67682112+Rares03@users.noreply.github.com>
2020-07-06 12:48:48 +0300

    Second sign up activity

--- app/src/main/java/com/example/nodedpit/MainActivity.java
+++ app/src/main/java/com/example/nodedpit/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawLayout;
     private ActionBarDrawerToggle mToggle;
+    private long backPressedTime;
+    private Toast backToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onBackPressed() {
 
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            super.onBackPressed();
+            return;
+        }
+        else{
+            backToast = Toast.makeText(MainActivity.this, "Press back again to exit",
+                    Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
 
 
 }
