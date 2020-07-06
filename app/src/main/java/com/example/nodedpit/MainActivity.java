37661922e083332994486db35bbe91947fda9c5b
xd <aexsas@outlook.com>
2020-07-06 12:18:31 +0300

    Login + SignUp + Drawer Menu

--- app/src/main/java/com/example/nodedpit/MainActivity.java
+++ app/src/main/java/com/example/nodedpit/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.nodedpit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView uid;
+    private DrawerLayout mDrawLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
 }
