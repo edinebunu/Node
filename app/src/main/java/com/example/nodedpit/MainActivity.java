52a7a6a53f78ca9d5ad8ea323d3e977fccde5157
xd <aexsas@outlook.com>
2020-07-07 14:36:06 +0300

    Login + SignUp + Drawer Menu + AddEvent + Profile

--- app/src/main/java/com/example/nodedpit/MainActivity.java
+++ app/src/main/java/com/example/nodedpit/MainActivity.java
@@ -2,21 +2,28 @@ package com.example.nodedpit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout mDrawLayout;
-    private ActionBarDrawerToggle mToggle;
+
+
     private long backPressedTime;
     private Toast backToast;
     String mUid;
@@ -28,6 +35,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawLayout = findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawLayout, toolbar, R.string.open, R.string.close);
+        mDrawLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
 
         Intent intent = getIntent();
         mUid = intent.getStringExtra("UID");
@@ -73,33 +95,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onBackPressed() {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.AddEvent:
+                openAddEvent();
+                break;
+            case R.id.profileButton:
+                openProfile();
+                break;
+        }
 
-        if (backPressedTime + 2000 > System.currentTimeMillis()) {
-            backToast.cancel();
-            super.onBackPressed();
-            return;
-        }
-        else{
-            backToast = Toast.makeText(MainActivity.this, "Press back again to exit",
-                    Toast.LENGTH_SHORT);
-            backToast.show();
-        }
-        backPressedTime = System.currentTimeMillis();
+        mDrawLayout.closeDrawer(GravityCompat.END);
+        return true;
     }
 
-    public void btnPressed(View view){
+    @Override
+    public void onBackPressed() {
+
+        if (mDrawLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawLayout.closeDrawer(GravityCompat.END);
+        } else {
+
+
+            if (backPressedTime + 2000 > System.currentTimeMillis()) {
+                backToast.cancel();
+                super.onBackPressed();
+                return;
+            } else {
+                backToast = Toast.makeText(MainActivity.this, "Press back again to exit",
+                        Toast.LENGTH_SHORT);
+                backToast.show();
+            }
+            backPressedTime = System.currentTimeMillis();
+        }
+    }
+
+    public void btnPressed(View view) {
         Intent intent = new Intent(this, CreateEvent.class);
-        intent.putExtra("UID",mUid);
+        intent.putExtra("UID", mUid);
         startActivity(intent);
     }
 
-    private void initRecyclerView(){
+    private void initRecyclerView() {
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(mNames, mDesc, this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    public void openAddEvent() {
+        Intent intent = new Intent(this, CreateEvent.class);
+        startActivity(intent);
+    }
+
+    public void openProfile() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
 
 }
